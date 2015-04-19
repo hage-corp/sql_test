94c7afc3f6e438c1e2c2ee926e4cec017d516eb0
washi-Z <washi@NishiwakiHajime-no-MacBook-Pro.local>
2015-04-19 21:56:54 +0900

    一旦、プロジェクトのみコミット

---
+++ sql−test/sql−test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sql−test
+//
+//  Created by NishiwakiHajime on 2015/04/19.
+//  Copyright (c) 2015年 NishiwakiHajime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
