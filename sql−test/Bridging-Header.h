806fabec5193657da011729154b58d9a4baa6a9c
washi-Z <washi@NishiwakiHajime-no-MacBook-Pro.local>
2015-04-29 22:05:31 +0900

    第二回ーでもなぜかINSERTでエラーが・・

---
+++ sql−test/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  sql−test
+//
+//  Created by NishiwakiHajime on 2015/04/20.
+//  Copyright (c) 2015年 NishiwakiHajime. All rights reserved.
+//
+
+#ifndef sql_test_Bridging_Header_h
+#define sql_test_Bridging_Header_h
+
+#import "sqlite3.h"
+
+#endif
